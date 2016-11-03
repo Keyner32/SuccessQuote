3015cc1ed319e45f482e0f7b7d05dd8effcfa302
Gabe Keyner <holyground32@gmail.com>
2016-11-03 11:11:37 -0700

    added adapter to bindviewholder

--- app/src/main/java/com/example/gabekeyner/successquote/Adapter.java
+++ app/src/main/java/com/example/gabekeyner/successquote/Adapter.java
@@ -2,7 +2,9 @@ package com.example.gabekeyner.successquote;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -17,6 +19,7 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
 
     private ArrayList<QuoteHelper> quotes;
     private Context context;
+    CardView cardView;
     int prevPos = 0;
 
     public Adapter(Context context, ArrayList<QuoteHelper> quotes) {
@@ -27,13 +30,28 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_view,parent,false);
+        return new ViewHolder(itemView);
     }
 
     @Override
     public void onBindViewHolder(final Adapter.ViewHolder holder, final int position) {
         final QuoteHelper quote = quotes.get(position);
+        holder.authorText.setText(quote.getAuthor());
+        holder.bodyText.setText(quote.getQuote_body());
+        holder.categoryText.setText(quote.getQuote_category());
 
+        cardView = (CardView) cardView.findViewById(R.id.cardView);
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDetailActivity(quote.getAuthor(),quote.getQuote_body(),quote.getQuote_category());
+            }
+        });
+
+        //TODO use animations but change them slightly
+//TODO FINISH PUTTING INFO ON BINDHOLDER in the one that goes on the card view
+        //TODO ONLY SHOW [CERTAIN AMOUNT OF BODY] AND [CATERGORY] AND [AUTHOR]
 
     }
 
